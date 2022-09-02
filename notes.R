2b8973c0da5e547cdbf196647af5401c1213ddca
Michael Ellsworth <52103104+miellsworth@users.noreply.github.com>
2022-09-02 07:43:02 -0600

    Update notes with ch 11.1

--- notes.R
+++ notes.R
@@ -582,3 +582,56 @@ keep_pred <- control_resamples(save_pred = TRUE, save_workflow = TRUE)
 set.seed(1003)
 rf_res <- rf_wflow %>% fit_resamples(resamples = ames_folds, control = keep_pred)
 
+# Creating multiple models with workflow sets
+library(tidymodels)
+tidymodels_prefer()
+
+basic_rec <- 
+  recipe(Sale_Price ~ Neighborhood + Gr_Liv_Area + Year_Built + Bldg_Type + 
+           Latitude + Longitude, data = ames_train) %>%
+  step_log(Gr_Liv_Area, base = 10) %>% 
+  step_other(Neighborhood, threshold = 0.01) %>% 
+  step_dummy(all_nominal_predictors())
+
+interaction_rec <- 
+  basic_rec %>% 
+  step_interact( ~ Gr_Liv_Area:starts_with("Bldg_Type_") ) 
+
+spline_rec <- 
+  interaction_rec %>% 
+  step_ns(Latitude, Longitude, deg_free = 50)
+
+preproc <- 
+  list(basic = basic_rec, 
+       interact = interaction_rec, 
+       splines = spline_rec
+  )
+
+lm_models <- workflow_set(preproc, list(lm = linear_reg()), cross = FALSE)
+lm_models
+
+# Resampling each of the models in turn
+lm_models <- 
+  lm_models %>% 
+  workflow_map("fit_resamples", 
+               # Options to `workflow_map()`: 
+               seed = 1101, verbose = TRUE,
+               # Options to `fit_resamples()`: 
+               resamples = ames_folds, control = keep_pred)
+lm_models
+
+# Collect rmse metric
+collect_metrics(lm_models) %>% 
+  filter(.metric == "rmse")
+
+# Add the random forest model to the workflow set
+four_models <- 
+  as_workflow_set(random_forest = rf_res) %>% 
+  bind_rows(lm_models)
+four_models
+
+# Visualize the confidence intervals of each model
+library(ggrepel)
+autoplot(four_models, metric = "rsq") +
+  geom_text_repel(aes(label = wflow_id), nudge_x = 1/8, nudge_y = 1/100) +
+  theme(legend.position = "none")
