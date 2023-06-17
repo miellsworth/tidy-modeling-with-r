4ea4af6de3bcb149d0b274035507dd0c2cc4539c
Michael Ellsworth <52103104+miellsworth@users.noreply.github.com>
2023-06-17 16:47:42 -0600

    Update notes.R

--- notes.R
+++ notes.R
@@ -2105,3 +2105,141 @@ vip_beans <-
     verbose = FALSE
   ) %>% 
   model_parts() 
+
+# Equivocal Results for Predictive Models
+# Simulate classification data
+library(tidymodels)
+tidymodels_prefer()
+
+simulate_two_classes <- 
+  function (n, error = 0.1, eqn = quote(-1 - 2 * x - 0.2 * x^2 + 2 * y^2))  {
+    # Slightly correlated predictors
+    sigma <- matrix(c(1, 0.7, 0.7, 1), nrow = 2, ncol = 2)
+    dat <- MASS::mvrnorm(n = n, mu = c(0, 0), Sigma = sigma)
+    colnames(dat) <- c("x", "y")
+    cls <- paste0("class_", 1:2)
+    dat <- 
+      as_tibble(dat) %>% 
+      mutate(
+        linear_pred = !!eqn,
+        # Add some misclassification noise
+        linear_pred = linear_pred + rnorm(n, sd = error),
+        prob = binomial()$linkinv(linear_pred),
+        class = ifelse(prob > runif(n), cls[1], cls[2]),
+        class = factor(class, levels = cls)
+      )
+    dplyr::select(dat, x, y, class)
+  }
+
+set.seed(1901)
+training_set <- simulate_two_classes(200)
+testing_set  <- simulate_two_classes(50)
+
+# Estimate a logistic regression model
+two_class_mod <- 
+  logistic_reg() %>% 
+  set_engine("stan", seed = 1902) %>% 
+  fit(class ~ . + I(x^2)+ I(y^2), data = training_set)
+print(two_class_mod, digits = 3)
+
+# Finding a balance between improving performance and enough reportable results
+test_pred <- augment(two_class_mod, testing_set)
+test_pred %>% head()
+
+library(probably)  # contains functions for equivocal zones
+
+lvls <- levels(training_set$class)
+
+test_pred <- 
+  test_pred %>%
+  # The buffer argument represents the bounds around 0.5 that make up the equivocal results
+  mutate(.pred_with_eqz = make_two_class_pred(.pred_class_1, lvls, buffer = 0.15))
+
+test_pred %>% count(.pred_with_eqz)
+
+# Confusion matrices for all data and data with reportable results only (equivocal results removed)
+# All data
+test_pred %>% conf_mat(class, .pred_class)
+
+# Reportable results only: 
+test_pred %>% conf_mat(class, .pred_with_eqz)
+
+# A function to change the buffer then compute performance.
+eq_zone_results <- function(buffer) {
+  test_pred <- 
+    test_pred %>% 
+    mutate(.pred_with_eqz = make_two_class_pred(.pred_class_1, lvls, buffer = buffer))
+  acc <- test_pred %>% accuracy(class, .pred_with_eqz)
+  rep_rate <- reportable_rate(test_pred$.pred_with_eqz)
+  tibble(accuracy = acc$.estimate, reportable = rep_rate, buffer = buffer)
+}
+
+# Evaluate a sequence of buffers and plot the results. 
+map(seq(0, .1, length.out = 40), eq_zone_results) %>% 
+  list_rbind() %>% 
+  pivot_longer(c(-buffer), names_to = "statistic", values_to = "value") %>% 
+  ggplot(aes(x = buffer, y = value, lty = statistic)) + 
+  geom_step(linewidth = 1.2, alpha = 0.8) + 
+  labs(y = NULL, lty = NULL)
+
+# Determining model applicability
+## loads both `Chicago` data set as well as `stations`
+data(Chicago)
+
+Chicago <- Chicago %>% select(ridership, date, one_of(stations))
+
+n <- nrow(Chicago)
+
+Chicago_train <- Chicago %>% slice(1:(n - 14))
+Chicago_test  <- Chicago %>% slice((n - 13):n)
+
+# Fit a standard linear model
+base_recipe <-
+  recipe(ridership ~ ., data = Chicago_train) %>%
+  # Create date features
+  step_date(date) %>%
+  step_holiday(date, keep_original_cols = FALSE) %>%
+  # Create dummy variables from factor columns
+  step_dummy(all_nominal()) %>%
+  # Remove any columns with a single unique value
+  step_zv(all_predictors()) %>%
+  step_normalize(!!!stations)%>%
+  step_pls(!!!stations, num_comp = 10, outcome = vars(ridership))
+
+lm_spec <-
+  linear_reg() %>%
+  set_engine("lm") 
+
+lm_wflow <-
+  workflow() %>%
+  add_recipe(base_recipe) %>%
+  add_model(lm_spec)
+
+set.seed(1902)
+lm_fit <- fit(lm_wflow, data = Chicago_train)
+
+# Determine how well the data fit on the test set
+res_test <-
+  predict(lm_fit, Chicago_test) %>%
+  bind_cols(
+    predict(lm_fit, Chicago_test, type = "pred_int"),
+    Chicago_test
+  )
+
+res_test %>% select(date, ridership, starts_with(".pred"))
+
+res_test %>% rmse(ridership, .pred)
+
+# Test the model on data a few years later
+res_2020 <-
+  predict(lm_fit, Chicago_2020) %>%
+  bind_cols(
+    predict(lm_fit, Chicago_2020, type = "pred_int"),
+    Chicago_2020
+  ) 
+
+res_2020 %>% select(date, contains(".pred"))
+
+res_2020 %>% select(date, ridership, starts_with(".pred"))
+
+res_2020 %>% rmse(ridership, .pred)
